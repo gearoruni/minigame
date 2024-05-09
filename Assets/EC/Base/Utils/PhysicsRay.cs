968a783e65819ca877bf42de0a3d16708f4eb700
HoshinoSak1 <870859934@qq.com>
2024-05-09 08:43:21 +0800

    单射线检测

--- Assets/EC/Base/Utils/PhysicsRay.cs
+++ Assets/EC/Base/Utils/PhysicsRay.cs
@@ -7,27 +7,57 @@ using static UnityEngine.UI.Image;
 public class PhysicsRay : MonoBehaviour
 {
     static LayerMask baseMask = LayerMask.GetMask("Wall");
-    public static Vector2 CheckCollision(Vector3 position,float x,float y,float radis,LayerMask layerMask = default(LayerMask))
-    {
-        Vector2 xdirection = new Vector2(x, 0);
-        Vector2 ydirection = new Vector2(0, y);
+    //public static Vector2 CheckCollision(Vector3 position,float x,float y,float radis,LayerMask layerMask = default(LayerMask))
+    //{
+    //    Vector2 xdirection = new Vector2(x, 0);
+    //    Vector2 ydirection = new Vector2(0, y);
 
-        float xdisctance = xdirection.magnitude;
-        float ydisctance = ydirection.magnitude;
+    //    float xdisctance = xdirection.magnitude;
+    //    float ydisctance = ydirection.magnitude;
+
+    //    Vector2 res = new Vector2();
+
+    //    RaycastHit2D xhit = Physics2D.Raycast(position, xdirection, xdisctance + radis, baseMask | layerMask);
+    //    if (xhit.collider != null)
+    //        res.x = position.x;
+    //    else
+    //        res.x = position.x + xdirection.normalized.x * xdisctance;
+
+    //    RaycastHit2D yhit = Physics2D.Raycast(position, ydirection, ydisctance + radis, baseMask | layerMask);
+    //    if (yhit.collider != null)
+    //        res.y = position.y;
+    //    else
+    //        res.y = position.y + ydirection.normalized.y * ydisctance;
+
+    //    return res;
+    //}
+    public static Vector2 CheckCollision(Vector3 position, float x, float y, float radis, LayerMask layerMask = default(LayerMask))
+    {
+        Vector2 direction = new Vector2(x, y);
+
+        float disctance = direction.magnitude;
+        
 
         Vector2 res = new Vector2();
 
-        RaycastHit2D xhit = Physics2D.Raycast(position, xdirection, xdisctance + radis, baseMask | layerMask);
-        if (xhit.collider != null)
-            res.x = position.x;
-        else
-            res.x = position.x + xdirection.normalized.x * xdisctance;
+        RaycastHit2D hit = Physics2D.Raycast(position, direction, disctance + radis, baseMask | layerMask);
+        if (hit.collider != null)
+        {
+            Vector2 normal = hit.normal.normalized;
 
-        RaycastHit2D yhit = Physics2D.Raycast(position, ydirection, ydisctance + radis, baseMask | layerMask);
-        if (yhit.collider != null)
-            res.y = position.y;
+            Vector3 projectForwardDirection = Vector3.ProjectOnPlane(-normal, Vector3.forward);
+            Vector3 projectLeftDirection = Vector3.ProjectOnPlane(-normal, Vector3.left);
+
+            Vector3 subForwardEnd = Vector2.Dot(hit.point - (Vector2)position, projectForwardDirection) * projectForwardDirection;
+            Vector3 subLeftEnd = Vector2.Dot(hit.point - (Vector2)position, projectLeftDirection) * projectLeftDirection;
+
+            position = (Vector2)position - (Vector2)(subForwardEnd + subLeftEnd).normalized * disctance / 2;
+            res = position;
+        }
         else
-            res.y = position.y + ydirection.normalized.y * ydisctance;
+        {
+            res = (Vector2)position + direction.normalized * disctance;
+        }
 
         return res;
     }
