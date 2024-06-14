b29740df753740dfe3a3c682fcfad70aada4ddbc
蒙果 <mengguo@shiyue.com>
2024-06-14 11:38:01 +0800

    调整交互弹窗

--- Assets/EC/Components/EntityComponents/InteractiveComponent.cs
+++ Assets/EC/Components/EntityComponents/InteractiveComponent.cs
@@ -66,7 +66,7 @@ public class InteractiveComponent : Component
         isInteracting = true;
         
         interactiveGO.SetActive(true);
-        interactiveGO.transform.position = this.entity.go.transform.position + new Vector3(0, 1.5f, 0); 
+        interactiveGO.transform.position = this.entity.go.transform.position + new Vector3(0, 0.5f, 0); 
     }
 
     public override void Update()
